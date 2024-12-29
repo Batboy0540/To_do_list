6335f38ea58bb8c46571e1ea3df7f6b68961f82b
Batboy0540 <parzivall8@duck.com>
2024-12-29 13:32:44 +0000

    final project

---
+++ app.py
@@ -0,0 +1,91 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Flask-Login asetukset
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+# Importoi tietokantamallit
+from models import User, Task
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+# Reitit
+@app.route('/')
+@login_required
+def index():
+    tasks = Task.query.filter_by(user_id=current_user.id).all()
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+@login_required
+def add_task():
+    title = request.form['title']
+    new_task = Task(title=title, user_id=current_user.id)
+    db.session.add(new_task)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:task_id>')
+@login_required
+def delete_task(task_id):
+    task = Task.query.get(task_id)
+    if task and task.user_id == current_user.id:
+        db.session.delete(task)
+        db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/update_status/<int:task_id>', methods=['POST'])
+@login_required
+def update_status(task_id):
+    task = Task.query.get(task_id)
+    if task and task.user_id == current_user.id:
+        task.status = request.form['status']
+        db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.check_password(password):
+            login_user(user)
+            return redirect(url_for('index'))
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        new_user = User(username=username)
+        new_user.set_password(password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+if __name__ == '__main__':
+    db.create_all()  # Luo tietokantataulut automaattisesti
+    app.run(debug=True)
